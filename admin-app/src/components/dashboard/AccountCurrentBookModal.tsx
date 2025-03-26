381f223d1d564ebb57ae5bebc4232dffca513997
Yakobus Iryanto Prasethio <yakobusiryantoprasethio@gmail.com>
2025-03-26 08:12:40 +0700

    fix: overflow issue on calendar component

--- admin-app/src/components/dashboard/AccountCurrentBookModal.tsx
+++ admin-app/src/components/dashboard/AccountCurrentBookModal.tsx
@@ -302,7 +302,7 @@ https://discord.gg/ywqTZSTwRY `);
                           </Button>
                         </PopoverTrigger>
                         <PopoverContent className="w-auto p-0">
-                          <div className="sm:flex">
+                          <div className="sm:flex overflow-auto h-96 xl:h-fit">
                             <Calendar
                               mode="single"
                               selected={field.value ?? undefined}
