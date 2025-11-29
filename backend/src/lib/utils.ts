6f6e1cb7770b3fa60ec82a12ef3b8f509d5ddf71
eiffelaqilaa <eiffelaqilaa@gmail.com>
2025-11-30 00:38:07 +0700

    feat: add booking-payment service layer

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -6,3 +6,21 @@ export const generateFilename = (originalName: string) => {
   const uuid = uuidv4();
   return `${uuid}${extension}`;
 };
+
+export const addDays = (date: Date, daysToAdd: number) => {
+  const d = new Date(date);
+  d.setDate(d.getDate() + daysToAdd);
+  return d;
+};
+
+export const addHours = (date: Date, hoursToAdd: number) => {
+  const d = new Date(date);
+  d.setHours(d.getHours() + hoursToAdd);
+  return d;
+};
+
+export const addMinutes = (date: Date, minutesToAdd: number) => {
+  const d = new Date(date);
+  d.setMinutes(d.getMinutes() + minutesToAdd);
+  return d;
+};
