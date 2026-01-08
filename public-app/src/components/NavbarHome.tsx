4fd4b1208248a7f4d5d1e258f543d2d1be2f340d
GerryNathanael <gerry.nathanael@teklogue.com>
2026-01-08 20:21:49 +0700

    Updata Navbar

--- public-app/src/components/NavbarHome.tsx
+++ public-app/src/components/NavbarHome.tsx
@@ -23,21 +23,61 @@ const Navbar = () => {
 
   return (
     <div className="fixed top-0 z-50 w-full bg-[#000000] shadow-md shadow-black/5">
-      <div className="mx-auto max-w-[1920px] pt-5 h-[84px] md:h-[80px] flex items-center justify-between px-5 md:px-14">
+      <div className="mx-auto max-w-[1920px] h-[84px] md:h-[80px] flex items-center justify-between px-5 md:px-14">
+        <div className="flex items-center gap-16">
+          <div className="relative md:pl-14">
+            <figure className="relative top-0 lg:max-w-[130px] sm:max-w-[130px] max-w-[130px] left-5">
+              <Image
+                src="/header/Logo Header Valforum.png"
+                alt="logo"
+                height={50}
+                width={130}
+                className="object-contain"
+              />
+            </figure>
+          </div>
+          {/* BRAND SWITCHER */}
+          <div className="relative ml-6">
+            <div className="flex items-center gap-2 px-3 py-2 rounded-2xl bg-gradient-to-r from-[#5a5a5a] to-[#2f2f2f] border border-white/20 shadow-inner">
 
-        {/* LOGO */}
-        <div className="relative md:pl-14">
-          <figure className="relative top-0 lg:max-w-[200px] sm:max-w-[150px] max-w-[150px] left-5">
-            <Image
-              src="/logo/Logo Valsewa 6 SVG.svg"
-              alt="logo"
-              height={300}
-              width={200}
-              className="object-contain"
-            />
-          </figure>
+              {/* ACTIVE - VALSEWA */}
+              <div className="flex items-center justify-center px-6 py-2 rounded-xl shadow-md cursor-pointer">
+                <Image
+                  src="/header/VALSEWA.png"
+                  alt="VALSEWA"
+                  width={130}
+                  height={28}
+                  className="object-contain"
+                />
+              </div>
+
+              {/* VALJUBEL */}
+              <div className="flex items-center justify-center px-6 py-2 rounded-xl hover:bg-white/10 transition cursor-pointer">
+                <Image
+                  src="/header/VALJUBEL.png"
+                  alt="VALJUBEL"
+                  width={130}
+                  height={28}
+                  className="object-contain"
+                />
+              </div>
+
+              {/* VALJOKI */}
+              <div className="flex items-center justify-center px-6 py-2 rounded-xl hover:bg-white/10 transition cursor-pointer">
+                <Image
+                  src="/header/VALJOKI.png"
+                  alt="VALJOKI"
+                  width={130}
+                  height={28}
+                  className="object-contain"
+                />
+              </div>
+
+            </div>
+          </div>
+
+          
         </div>
-
         {/* NAV RIGHT SIDE */}
         <div className="md:pr-14 pr-7 flex items-center gap-4">
 
