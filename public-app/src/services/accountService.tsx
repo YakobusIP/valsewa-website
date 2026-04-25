3730dc1630ed6c6cdaaa15c60578cb1a0fa1d323
hcarissa <13520164@std.stei.itb.ac.id>
2026-04-25 07:37:01 +0700

    fix params on public app

--- public-app/src/services/accountService.tsx
+++ public-app/src/services/accountService.tsx
@@ -101,8 +101,8 @@ function buildAccountsPublicQuery(params: AccountsPublicParams) {
   if (params.direction) sp.set("direction", params.direction);
 
   // boolean: if you want "all" to omit this param, just pass undefined from UI
-  if (typeof params.competitive_only === "boolean") {
-    sp.set("competitive_only", String(params.competitive_only));
+  if (typeof params.compe_only === "boolean") {
+    sp.set("compe_only", String(params.compe_only));
   }
 
   // arrays as repeated keys: tiers=B&tiers=C ...
